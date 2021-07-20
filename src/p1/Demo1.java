7d752f0fb13ae50b28a5724edcef112b80e70006
Suneela <suneela@Suneela-MBP.local>
2021-07-20 18:58:33 +0530

    first commit

---
+++ src/p1/Demo1.java
@@ -0,0 +1,18 @@
+package p1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		System.out.println("Selenium");
+		
+		String key="webdriver.chrome.driver";
+		String value="/Users/suneela/EclipeWorkSpace/Automation/SelAutomation/Drivers/chromedriver";
+		System.setProperty(key, value);
+		ChromeDriver driver= new ChromeDriver();
+		driver.close();
+
+	}
+
+}
